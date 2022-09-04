1e39e73d7b856c318cd51afae0e6fae8f5fd348d
zlewchan <x@zlew.tf>
2022-09-04 19:53:39 +0200

    sepolicy: Initialize HekaOS
    Signed-off-by: zlewchan <x@zlew.tf>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Arrow
+# inherit from Heka
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -16,20 +16,20 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/public
+    device/heka/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/private
+    device/heka/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/system
+    device/heka/sepolicy/common/dynamic \
+    device/heka/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/vendor
+    device/heka/sepolicy/common/dynamic \
+    device/heka/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/arrow/sepolicy/legacy-common/sepolicy.mk
+-include device/heka/sepolicy/legacy-common/sepolicy.mk
